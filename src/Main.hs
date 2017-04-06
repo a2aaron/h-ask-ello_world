f1f2b02ded7f633b9430fd83308d3e6df49f2eda
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-04-06 11:36:05 -0400

    Play around with simple functions

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,25 @@
 module Main where
 
 main :: IO ()
-main = print a_big_list
+main = do print (successor (add 54 4))
+          print (add 100 (predecessor 100))
+
 
 a_list = [1, 2, 3]
 
-a_list2 = [1..100]
+a_list2 = [1..10]
 
-a_big_list = [1..]
+a_big_list = [1..10000]
 
+a_concated_list = a_list ++ a_list2
 
+successor :: Integer -> Integer
+successor x = x + 1
+
+predecessor :: Integer -> Integer
+predecessor x = x - 1
+
+add :: Integer -> Integer -> Integer
+add a 0 = a
+add a b = add (successor a) (b - 1)
 
